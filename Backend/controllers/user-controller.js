6ad3fd05e4fc3e11c27de65fa0aaa51fa2a485f2
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-13 22:31:31 +0300

    Update user-controller.js with user registration logic

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -1,5 +1,69 @@
-// User Controller
+import asyncHandler from "express-async-handler";
+import prisma from "../config/db.js";
 
 // Implement user controller logic here
+
+/** 
+@controller Get all users
+@description Create a new user
+@route POST /api/users
+@access private
+@method POST
+*/
+export const UserRegister = asyncHandler(async (req, res) => {
+  const { email, name, password } = req.body;
+
+  if(!email || !name || !password){
+    
+  }
+});
+
+
+// get all users controller
+/**
+@controller getUsersController
+@description Get all users
+@route GET /api/users
+@access private
+@method GET
+ */
+
+
+// get user by id controller
+/**
+@controller getUserByIdController  
+@description Get user by id
+@route GET /api/users/:id
+@access private
+@method GET
+ */
+
+
+// update user controller
+/**
+@controller updateUserController
+@description Update user
+@route PUT /api/users/:id
+@access private
+@method PUT
+ */
+
+// delete user controller
+/**
+@controller deleteUserController
+@description Delete user
+@route DELETE
+@access private
+@method DELETE
+  */
+
+// login user controller
+/**
+@controller loginUser
+@description Login user
+@route POST /api/users/login
+@access public
+@method POST
+ */
 
 // End of User Controller
