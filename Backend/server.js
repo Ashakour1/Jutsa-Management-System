bbf93c83f26a33cfb974ad5afec4252b5775f5b6
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-13 22:31:55 +0300

    Update server.js to use Express and error middleware

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import dotenv from "dotenv"
-import errorHandler from './middlewares/error-middleware.js';
+import express from "express";
+import dotenv from "dotenv";
+import errorHandler from "./middlewares/error-middleware.js";
+import userRoutes from "./routes/user-routes.js";
 dotenv.config();
 const app = express();
 
 const PORT = 5000;
-app.use(express.json());
-app.use(errorHandler)
 
-app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.use(express.json());
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
