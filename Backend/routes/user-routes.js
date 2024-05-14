ef6b41b7a2e7403bfc8513a4fa32c827513067ec
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-14 17:02:10 +0300

    Add user registration endpoint to user-routes.js

--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -5,10 +5,11 @@ import express from "express";
 const router = express.Router();
 
 // Import user controller
-import {getUsers} from "../controllers/user-controller.js";
+import {getUsers,UserRegister} from "../controllers/user-controller.js";
 
 // Get all users
 router.get("/",getUsers)
+router.post("/auth/reg",UserRegister)
 
 // Export router
 export default router;
