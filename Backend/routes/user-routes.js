6236153f55e144370f4dde6d64857d4b1c521267
Zakarie <116062502+zakariem@users.noreply.github.com>
2024-05-14 15:34:59 +0300

    Refactor user routes to use getUsers function

--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -5,14 +5,10 @@ import express from "express";
 const router = express.Router();
 
 // Import user controller
-import * as UserController from "../controllers/user-controller.js";
+import {getUsers} from "../controllers/user-controller.js";
 
 // Get all users
-router.get("/", ()=>{
-    UserController.getUsers();
-
-    return console.log("Get all users hhhh");
-});
+router.get("/",getUsers)
 
 // Export router
 export default router;
