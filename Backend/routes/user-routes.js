93c2d9935f38db46a0951eead63ea9271edf462e
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-14 19:56:42 +0300

    Refactor deleteUser endpoint to handle invalid user IDs

--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -5,15 +5,12 @@ import express from "express";
 const router = express.Router();
 
 // Import user controller
-
 import {getUsers,UserRegister,getUser,deleteUser} from "../controllers/user-controller.js";
 
-// Get all users
+
 router.get("/",getUsers)
 router.post("/auth/reg",UserRegister)
 router.get("/:id",getUser)
-
-//delete a user
 router.delete('/:id',deleteUser)
 
 
