f8e3a9c43e4b1a2344897151d77dc29bb4c2b223
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-14 17:17:56 +0300

    Add getUser endpoint to user-routes.js

--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -5,11 +5,12 @@ import express from "express";
 const router = express.Router();
 
 // Import user controller
-import {getUsers,UserRegister} from "../controllers/user-controller.js";
+import {getUsers,UserRegister,getUser} from "../controllers/user-controller.js";
 
 // Get all users
 router.get("/",getUsers)
 router.post("/auth/reg",UserRegister)
+router.get("/:id",getUser)
 
 // Export router
 export default router;
