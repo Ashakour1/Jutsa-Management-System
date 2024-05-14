8b81b9a311e002ee0376add587096d478b5a5c56
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-15 01:46:21 +0300

    Refactor user-routes.js to include updateUser endpoint

--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -5,15 +5,21 @@ import express from "express";
 const router = express.Router();
 
 // Import user controller
-import {getUsers,UserRegister,getUser,deleteUser, loginUser} from "../controllers/user-controller.js";
+import {
+  getUsers,
+  UserRegister,
+  getUser,
+  deleteUser,
+  loginUser,
+  updateUser,
+} from "../controllers/user-controller.js";
 
-
-router.get("/",getUsers)
-router.post("/auth/reg",UserRegister)
-router.post("/auth/login",loginUser)
-router.get("/:id",getUser)
-router.delete('/:id',deleteUser)
-
+router.get("/", getUsers);
+router.post("/auth/reg", UserRegister);
+router.put("/edit/:id", updateUser);
+router.post("/auth/login", loginUser);
+router.get("/:id", getUser);
+router.delete("/:id", deleteUser);
 
 // Export router
 export default router;
