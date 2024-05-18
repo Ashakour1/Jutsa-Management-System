3159cd653f5ff916b50d54c314e4755f85773522
ahmed-dalab <ahmedabdihussein489@gmail.com>
2024-05-18 17:30:23 +0100

    Added Footer section

---
+++ Frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='imgContainer'>
+        <img className='img' src="https://s3-alpha-sig.figma.com/img/068b/99c0/0830ee1a5638e2be2f69e29a65af3aea?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Wi96plOyTyd6h6NgBQf9RuqY0SOD4Mt8htDxyDXeVj10lnRWk39u6Vunz6HjgxQ93RiU8BX5NkbOEO2p-hSI9sbEocsaJQ-t43e9~6n5jqBM-lAHlqYo4boFjnsPpn9ZExVSFzQV6G392r~1U0NY8xw1Ey-nZI~pgd08cjXKXON2VBUdEkWQiEt2ZvFMeoS2NeV-55ZE1oVFRjAz05V0FoZu72HOCSF1cMp4rifhSLOcvXl0IOybkZ~3wWgDIuwOPmlopwfH1hwPSleSQm4grzdBv1~ebrxeqNXEpXHSWlpef~0J1tOMr4PTlM2sXoUPgsuQ0o1Xx3nXwb1rmCf2oA__" alt="img" />
+        </div>
+          <hr />
+        <p>@ 2024 Jutsa All right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
