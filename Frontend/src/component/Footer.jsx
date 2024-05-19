c49e50ff96f70bb9f74c83a45205accb3d7c09b3
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-19 20:40:12 +0300

    Update footer component styling and add logo image

--- Frontend/src/component/Footer.jsx
+++ Frontend/src/component/Footer.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='mt-auto'>
-      <div className='flex flex-col justify-center items-center'>
-        <img className='w-80 h-52 sm:w-50 sm:h-30' src="https://s3-alpha-sig.figma.com/img/068b/99c0/0830ee1a5638e2be2f69e29a65af3aea?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Wi96plOyTyd6h6NgBQf9RuqY0SOD4Mt8htDxyDXeVj10lnRWk39u6Vunz6HjgxQ93RiU8BX5NkbOEO2p-hSI9sbEocsaJQ-t43e9~6n5jqBM-lAHlqYo4boFjnsPpn9ZExVSFzQV6G392r~1U0NY8xw1Ey-nZI~pgd08cjXKXON2VBUdEkWQiEt2ZvFMeoS2NeV-55ZE1oVFRjAz05V0FoZu72HOCSF1cMp4rifhSLOcvXl0IOybkZ~3wWgDIuwOPmlopwfH1hwPSleSQm4grzdBv1~ebrxeqNXEpXHSWlpef~0J1tOMr4PTlM2sXoUPgsuQ0o1Xx3nXwb1rmCf2oA__" alt="img" />
-        </div>
-          <hr />
-        <p className='flex flex-col justify-center items-center mb-6 mt-6'>@ 2024 Jutsa All right Reserved</p>
-    </div>
-  )
-}
+    <div className="mt-auto">
+      <div className="flex flex-col justify-center items-center">
+        <img className="h-15 sm:w-50 sm:h-30" src="/logo.png" alt="img" />
+      </div>
+      <div className="text-black font-bold h-10">
+        <hr className="border-t border-1.5 border-black"/>
+      </div>
 
-export default Footer
+      <p className="flex flex-col justify-center items-center mb-6 mt-6">
+        @ 2024 Jutsa All right Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
