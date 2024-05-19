94d59697344c7b7a92f761520315efbe4b8037ee
M40 <modev.404@gmail.com>
2024-05-19 08:03:41 +0300

    Created Competitor controller with DeleteCompetitor functionality

---
+++ Backend/controllers/competitorController.js
@@ -0,0 +1,36 @@
+import asyncHandler from "express-async-handler";
+import prisma from "../config/db";
+
+//Delete competitor
+export const  deleteCompetitor = asyncHandler(async(req,res)=>{
+    const { id } = req.params
+    const competitor = await prisma.competitor.findUnique({
+        where: {
+            id
+        }
+    })
+
+    if (!competitor){
+        res.status(404)
+        throw new Error("Unable to find: not registered competitor")
+    }
+
+    const deletedComp = await prisma.competitor.delete({
+        where: {
+            id
+        }
+    })
+
+    if (!deletedComp) {
+        res.status(400)
+        throw new Error("Unable to delete: unexpected error occurred")
+    }
+
+    res.status(200).json({
+        success: true,
+        error: null,
+        data:{
+            message:'Deleted successfully'
+        },
+    });
+})
