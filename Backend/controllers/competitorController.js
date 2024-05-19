758ac3e8032cea2e563f81be033564486f5f7f1d
M40 <modev.404@gmail.com>
2024-05-19 09:09:26 +0300

    Refactor competitor controller and added checking is objectID is valid

--- Backend/controllers/competitorController.js
+++ Backend/controllers/competitorController.js
@@ -1,9 +1,16 @@
 import asyncHandler from "express-async-handler";
-import prisma from "../config/db";
+import prisma from "../config/db.js";
+import { ObjectId } from "mongodb";
 
 //Delete competitor
 export const  deleteCompetitor = asyncHandler(async(req,res)=>{
     const { id } = req.params
+
+    if(!ObjectId.isValid(id)){
+        res.status(400)
+        throw new Error('Please enter a valid id')
+    }
+    
     const competitor = await prisma.competitor.findUnique({
         where: {
             id
