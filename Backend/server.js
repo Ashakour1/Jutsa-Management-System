3a693a0812ec20fcbac540a05598411bb8d09c2e
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-19 20:38:49 +0300

    Add competitor registration route

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import errorHandler from "./middlewares/error-middleware.js";
 import userRoutes from "./routes/user-routes.js";
+import competitorRoutes from "./routes/competitor-routes.js";
 dotenv.config();
 const app = express();
 
 const PORT = 5000;
 
-// Middleware 
+// Middleware
 app.use(express.json());
 
 // Routes
 app.use("/api/users", userRoutes);
+app.use("/api/competitors", competitorRoutes);
 
 // Error handler middleware
 app.use(errorHandler);
