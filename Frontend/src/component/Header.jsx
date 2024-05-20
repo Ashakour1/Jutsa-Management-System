e4f810385a0367f79bfb4889ee704cd9ee3ba35b
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-20 18:51:05 +0300

    Update Header component with responsive design and font styling

--- Frontend/src/component/Header.jsx
+++ Frontend/src/component/Header.jsx
@@ -4,23 +4,23 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="bg-white shadow-md h-18">
-      <div className="max-w-[1040px] mx-auto h-full flex items-center justify-between">
+      <div className="max-w-[1140px] mx-auto h-full flex items-center justify-between">
         <div className="flex items-center space-x-4">
-          <Link className="" href="#">
+          <Link className="font-medium" href="#">
             Home
           </Link>
-          <Link className="" href="#">
+          <Link className="font-medium" href="#">
             About
           </Link>
-          <Link className="" href="#">
-            FAQ
+          <Link className="font-medium" href="#">
+            FaQ
           </Link>
         </div>
-        <div className="flex-grow flex justify-center">
-          <img src="./logo.png" alt="logo" className="h-full pr-20" />
+        <div className="flex flex-col justify-center items-center">
+          <img src="./logo.png" alt="logo" className="h-full pr-6" />
         </div>
         <div>
-          <button className="py-2 px-4 bg-customGreen rounded-sm">
+          <button className="py-2 px-4 bg-customGreen text-customBlue rounded-sm">
             Register Now
           </button>
         </div>
