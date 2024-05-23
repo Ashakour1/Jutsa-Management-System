b74e5732c7b926421173bea6af28f709c6099faa
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-23 21:39:19 +0300

    Update benefits page with improved layout and content

--- Frontend/src/pages/benefits.jsx
+++ Frontend/src/pages/benefits.jsx
@@ -1,54 +1,113 @@
+import { MdShowChart } from "react-icons/md";
+import { MdConnectWithoutContact } from "react-icons/md";
+import { VscFeedback } from "react-icons/vsc";
+import { GiInspiration } from "react-icons/gi";
+import { CgCommunity } from "react-icons/cg";
+import { MdExplore } from "react-icons/md";
+
 const Benefits = () => {
   return (
-    <section className="max-w-[1140px] mx-auto h-screen">
-      <div className="py-5 h-svh flex-col lg:flex-row flex justify-between gap-6 md:gap-5 md:items-center">
-        <div className="benefits-text basis-2/4 px-3 lg:px-0 flex flex-col gap-5">
-          <div className=" space-y-3">
-            <h1 className=" font-bold text-2xl lg:text-4xl">
-              Participate in IT-DAY: <br /> Share Your Projects and Ideas
-            </h1>
-            <p className=" leading-7 text-lg">
-              Join us for IT-DAY at Jamhuuriya University and showcase your
-              innovative projects to a community of tech enthusiasts. As a
-              Computer Science student, this is your opportunity to share your
-              skills, connect with industry experts,
+    <section class="bg-white text-black">
+      <div class="mx-auto max-w-contain px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div class="mx-auto max-w-[700px] text-center">
+          <h2 class="text-2xl font-semibold sm:text-4xl">
+            Unlock Opportunities by Showcasing Your Skills at IT-DAY!
+          </h2>
+
+          <p class="mt-4 text-gray-500">
+            Showcasing your skills at IT-DAY offers many benefits. Present your
+            projects to classmates, teachers, industry experts, and fellow
+            students. Gain feedback, recognition, and network with
+            professionals. Collaborate with peers, explore career paths, and get
+            inspired. Don’t miss this chance to shine at IT-DAY!
+          </p>
+        </div>
+
+        <div class="mt-8 grid grid-cols-1  gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <div
+            class="flex flex-col text-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-customGreen hover:shadow-green-100"
+            href="#"
+          >
+            <MdShowChart className="text-customGreen text-5xl text-center items-center" />
+
+            <h2 class="mt-4 text-xl font-bold text-black">Show Your Talent</h2>
+
+            <p class="mt-1 text-sm text-gray-500">
+              Display your projects to classmates, teachers, and industry
+              experts.
             </p>
           </div>
-          <div className=" flex items-center gap-5 flex-col  md:flex-row">
-            <div className=" md:basis-2/5 ">
-              <h3 className="font-bold text-xl lg:text-xl">
-                Skill Development
-              </h3>
-              <p className=" text-lg leading-6">
-                Present your projects and demonstrate your technical
-                capabilities to peers, faculty.
-              </p>
-            </div>
-            <div className="md:basis-2/5 ">
-              <h3 className="font-bold text-lg lg:text-xl">
-                Networking Opportunities
-              </h3>
-              <p className="text-lg leading-6">
-                Connect with like-minded professionals and expand your
-                professional network.
-              </p>
-            </div>
+          <div
+            class="flex flex-col text-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-customGreen hover:shadow-green-100"
+            href="#"
+          >
+            <MdConnectWithoutContact className="text-customGreen text-5xl text-center items-center" />
+
+            <h2 class="mt-4 text-xl font-bold text-black">Build Connections</h2>
+
+            <p class="mt-1 text-sm text-gray-500">
+              Meet professionals who can help with future job opportunities.
+            </p>
           </div>
-          <div className="flex items-center gap-4">
-            <button className=" py-2 px-5 bg-[#2BB77B] text-[#0E103F] cursor-pointer hover:bg-transparent hover:border hover:border-[#2BB77B] hover:text-[#2BB77B] transition">
-              Register Now
-            </button>
-            <button className="py-2 px-5 border  border-[#2BB77B]  text-[#2BB77B] cursor-pointer  hover:border-none hover:bg-[#2BB77B] transition hover:text-[#0E103F]">
-              Register Now
-            </button>
+          <div
+            class="flex flex-col text-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-customGreen hover:shadow-green-100"
+            href="#"
+          >
+            <VscFeedback className="text-customGreen text-5xl text-center items-center" />
+
+            <h2 class="mt-4 text-xl font-bold text-black">Receive Feedback</h2>
+
+            <p class="mt-1 text-sm text-gray-500">
+              Get valuable advice on how to improve your projects..
+            </p>
+          </div>
+          <div
+            class="flex flex-col text-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-customGreen hover:shadow-green-100"
+            href="#"
+          >
+            <CgCommunity className="text-customGreen text-5xl text-center items-center" />
+
+            <h2 class="mt-4 text-xl font-bold text-black">
+              Improve Communication
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-500">
+              Practice explaining your projects to a diverse audience.
+            </p>
+          </div>
+          <div
+            class="flex flex-col text-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-customGreen hover:shadow-green-100"
+            href="#"
+          >
+            <MdExplore className="text-customGreen text-5xl text-center items-center" />
+
+            <h2 class="mt-4 text-xl font-bold text-black">Explore Careers</h2>
+
+            <p class="mt-1 text-sm text-gray-500">
+              Discover different career paths in technology.
+            </p>
+          </div>
+          <div
+            class="flex flex-col text-center items-center rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-customGreen hover:shadow-green-100"
+            href="#"
+          >
+            <GiInspiration className="text-customGreen text-5xl text-center items-center" />
+
+            <h2 class="mt-4 text-xl font-bold text-black">Get Inspired</h2>
+
+            <p class="mt-1 text-sm text-gray-500">
+              See innovative projects from other students and gain new ideas.
+            </p>
           </div>
         </div>
-        <div className="img h-3/4 w-full lg:w-2/4 -order-1 lg:order-2">
-          <img
-            className="w-full h-full rounded-md"
-            src="../../public/imgs/Benifits_img.svg"
-            alt="Benefit section img"
-          />
+
+        <div class="mt-12 text-center">
+          <a
+            href="#"
+            class="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
+          >
+            Get Started Today
+          </a>
         </div>
       </div>
     </section>
