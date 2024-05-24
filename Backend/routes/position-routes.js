38c9b181b2c3e718c7014a3fd69b9c0edcdc8fd2
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-24 21:37:11 +0300

    Refactor position routes to use ES6 import syntax

--- Backend/routes/position-routes.js
+++ Backend/routes/position-routes.js
@@ -1,1 +1,17 @@
-// position routes with the specified controller
+import express from "express";
+
+import {
+  registerPosition,
+  getAllPositions,
+  deletePosition,
+  updatePosition,
+} from "../controllers/position-controller.js";
+
+const router = express.Router();
+
+router.get("/", getAllPositions);
+router.post("/", registerPosition);
+router.put("/:id", updatePosition);
+router.delete("/:id", deletePosition);
+
+export default router;
