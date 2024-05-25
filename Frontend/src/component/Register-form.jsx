a818ae28ad57ca17bd3b8dc64b0577eac0e684e8
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-25 22:35:09 +0300

    refactor: Update npm dependencies and add CORS package

--- Frontend/src/component/Register-form.jsx
+++ Frontend/src/component/Register-form.jsx
@@ -1,10 +1,66 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "sonner";
 const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    number: "",
+    email: "",
+    semester: "",
+    skill: "",
+    className: "",
+    type: "",
+    projectName: "",
+    technologies: "",
+  });
+  console.log(formData);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const clearText = () => {
+    setFormData({
+      name: "",
+      number: "",
+      email: "",
+      semester: "",
+      skill: "",
+      className: "",
+      type: "",
+      projectName: "",
+      technologies: "",
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/competitors",
+        formData
+      );
+      toast.success("Registration successful");
+      clearText();
+      // console.log(response.data);
+    } catch (err) {
+      console.log(err);
+      if(err){
+        toast.error(err.response.data.message)
+      }else{
+        toast.error("An error occured")
+      }
+    }
+  };
+
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100 py-20 px-4">
       <div className="w-[800px] rounded-lg mx-auto bg-white text-black p-8 shadow-lg ">
-        <h1 className="my-4 text-3xl font-bold tracking-tight text-gray-900">
+        <h1 className="my-4 text-3xl font-bold tracking-tight text-black">
           Registration
         </h1>
         <p className="mb-4 text-gray-700">
@@ -21,10 +77,13 @@ const RegisterForm = () => {
                 Name
               </label>
               <input
-                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-primary focus:ring-primary dark:text-gray-50"
+                onChange={handleChange}
+                value={formData.name}
+                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-black focus:border-primary focus:ring-primary"
                 id="name"
                 placeholder="Enter your name"
                 type="text"
+                name="name"
               />
             </div>
             <div className="flex flex-col">
@@ -35,10 +94,13 @@ const RegisterForm = () => {
                 Phone Number
               </label>
               <input
-                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-primary focus:ring-primary   dark:text-gray-50"
+                onChange={handleChange}
+                value={formData.number}
+                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-black focus:border-primary focus:ring-primary  "
                 id="phone"
                 placeholder="Enter your phone number"
                 type="tel"
+                name="number"
               />
             </div>
           </div>
@@ -51,10 +113,13 @@ const RegisterForm = () => {
                 Email
               </label>
               <input
-                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-primary focus:ring-primary   dark:text-gray-50"
+                onChange={handleChange}
+                value={formData.email}
+                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-black focus:border-primary focus:ring-primary  "
                 id="email"
                 placeholder="Enter your email"
                 type="email"
+                name="email"
               />
             </div>
             <div className="flex flex-col">
@@ -65,14 +130,15 @@ const RegisterForm = () => {
                 Semester
               </label>
               <select
+                value={formData.semester}
+                onChange={handleChange}
                 className="rounded-md border  p-2 text-sm text-black focus:border-primary focus:ring-primary"
                 id="semester"
                 placeholder="Enter your semester"
                 type="number"
+                name="semester"
               >
-                <option disabled selected>
-                  Select Semester
-                </option>
+                <option value>Select Semester</option>
                 <option value="1">1st Semester</option>
                 <option value="2">2nd Semester</option>
                 <option value="3">3rd Semester</option>
@@ -93,10 +159,13 @@ const RegisterForm = () => {
                 Class Name
               </label>
               <input
-                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-primary focus:ring-primary   dark:text-gray-50"
+                onChange={handleChange}
+                value={formData.className}
+                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-black focus:border-primary focus:ring-primary   "
                 id="class-name"
                 placeholder="Enter your class name"
                 type="text"
+                name="className"
               />
             </div>
             <div className="flex flex-col">
@@ -107,16 +176,17 @@ const RegisterForm = () => {
                 Type
               </label>
               <select
+                value={formData.type}
+                name="type"
+                onChange={handleChange}
                 className="rounded-md border border-gray-300 p-2 text-sm text-black focus:border-primary focus:ring-primary  "
                 id="type"
                 placeholder="Enter your type"
                 type="text"
               >
-                <option disabled selected>
-                  Select Type
-                </option>
+                <option value>Select Type</option>
                 <option value="Network">Network</option>
-                <option value="Programming">Web Development</option>
+                <option value="webdevelopment">Web Development</option>
                 <option value="Mobile Application">Mobile Application</option>
                 <option value="Graphic Design">Graphic Design</option>
                 <option value="typing">Typing </option>
@@ -132,10 +202,13 @@ const RegisterForm = () => {
                 Skill
               </label>
               <input
-                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-primary focus:ring-primary   dark:text-gray-50"
+                value={formData.skill}
+                onChange={handleChange}
+                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-black focus:border-primary focus:ring-primary  "
                 id="skill"
                 placeholder="eg: backend developer etc."
                 type="text"
+                name="skill"
               />
             </div>
             <div className="flex flex-col">
@@ -146,10 +219,13 @@ const RegisterForm = () => {
                 Project Name
               </label>
               <input
-                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-primary focus:ring-primary  dark:text-gray-50"
+                value={formData.project}
+                onChange={handleChange}
+                className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-black focus:border-primary focus:ring-primary "
                 id="project-name"
                 placeholder="Enter your project name"
                 type="text"
+                name="projectName"
               />
             </div>
           </div>
@@ -161,13 +237,17 @@ const RegisterForm = () => {
               Technologies
             </label>
             <input
-              className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-primary focus:ring-primary   dark:text-gray-50"
+              value={formData.technologies}
+              onChange={handleChange}
+              name="technologies"
+              className="rounded-md border border-gray-300 bg-gray-50 p-2 text-sm text-black focus:border-primary focus:ring-primary  "
               id="technologies"
               placeholder="Enter the technologies used"
               type="text"
             />
           </div>
           <button
+            onClick={handleSubmit}
             className="w-full rounded-md bg-customBlue px-4  text-sm font-medium text-white  py-3"
             type="submit"
           >
