8fd8ea012030844a9973a31585baee482e62b5c9
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-25 21:17:34 +0300

    Add FAQ page component

---
+++ Frontend/src/pages/FAQ-page.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+
+const FAQPage = () => {
+  return (
+    <div className="mt-20 max-w-[840px] mx-auto">
+      <div className="space-y-4">
+        <h1 className="text-3xl font-semibold text-gray-900">FAQs</h1>
+
+        <details
+          className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+          open
+        >
+          <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+            <h2 className="text-lg font-medium text-gray-900">
+              What is IT-DAY?
+            </h2>
+
+            <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+            IT-DAY is a special event for students in the IT faculty, including
+            the departments of Computer Application, Network, and Multimedia.
+            It's a day where students showcase their skills and knowledge
+            through various projects. IT-DAY highlights achievements and serves
+            as inspiration for new students, encouraging them to contribute
+            creatively to society and technology.
+          </p>
+        </details>
+
+        <details className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+          <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+            <h2 className="text-lg font-medium text-gray-900">
+              What You Need to Participate in IT-DAY?
+            </h2>
+
+            <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+            Completed Project: Develop a project related to your department
+            (Computer Application, Network, or Multimedia). <br /> Presentation:
+            Create and practice a clear presentation with visual aids to explain
+            your project and its impact.
+          </p>
+        </details>
+        <details className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+          <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+            <h2 className="text-lg font-medium text-gray-900">
+              Who is eligible to participate in IT-DAY?{" "}
+            </h2>
+
+            <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+            Anyone from the IT faculty, including students, faculty, staff,
+            alumni, and industry professionals, can participate in IT-DAY.
+          </p>
+        </details>
+        <details className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
+          <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+            <h2 className="text-lg font-medium text-gray-900">
+              How can participating in IT-DAY benefit students in the IT
+              faculty?
+            </h2>
+
+            <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </summary>
+
+          <p className="mt-4 leading-relaxed text-gray-700">
+            Participating in IT-DAY offers students several benefits. It
+            provides an opportunity to showcase their projects and skills to a
+            wider audience, including potential employers and collaborators.
+            Additionally, students can receive valuable feedback on their work,
+            network with industry professionals, and gain inspiration from
+            peers' projects
+          </p>
+        </details>
+      </div>
+    </div>
+  );
+};
+
+export default FAQPage;
