5ae1ea5f886d96fd668ecf2717d333f80854bd33
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-26 17:09:53 +0300

    Update server.js to enable CORS for specified methods and origin

--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,27 @@
 import express from "express";
-// import dotenv from "dotenv";
-// import errorHandler from "./middlewares/error-middleware.js";
-// import userRoutes from "./routes/user-routes.js";
-// import competitorRoutes from "./routes/competitor-routes.js";
-// import cors from "cors";
-// dotenv.config();
+import dotenv from "dotenv";
+import errorHandler from "./middlewares/error-middleware.js";
+import userRoutes from "./routes/user-routes.js";
+import competitorRoutes from "./routes/competitor-routes.js";
+import cors from "cors";
+dotenv.config();
 const app = express();
 
 const PORT = 5000;
 
-// app.use(
-//   cors({
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     origin: "https://jutsa-management-system.vercel.app/",
-//   })
-// );
-// // Middleware
-// app.use(express.json());
+app.use(cors());
+// Middleware
+app.use(express.json());
 
-// // Routes
-// app.use("/api/users", userRoutes);
-// app.use("/api/competitors", competitorRoutes);
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/competitors", competitorRoutes);
 app.get("/", (req, res) => {
-  res.json({
-    message: "You have been authenticated to access this website",
-  });
+  res.send("API is running...");
 });
 
-// // Error handler middleware
-// app.use(errorHandler);
+// Error handler middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
