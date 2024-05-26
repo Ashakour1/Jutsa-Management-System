73289a300e0c84e5ba14ac894cec8188514f18cf
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-26 15:14:57 +0300

    chore: Enable CORS for specified methods and origin in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,32 +1,32 @@
 import express from "express";
-import dotenv from "dotenv";
-import errorHandler from "./middlewares/error-middleware.js";
-import userRoutes from "./routes/user-routes.js";
-import competitorRoutes from "./routes/competitor-routes.js";
-import cors from "cors";
-dotenv.config();
+// import dotenv from "dotenv";
+// import errorHandler from "./middlewares/error-middleware.js";
+// import userRoutes from "./routes/user-routes.js";
+// import competitorRoutes from "./routes/competitor-routes.js";
+// import cors from "cors";
+// dotenv.config();
 const app = express();
 
 const PORT = 5000;
 
-app.use(
-  cors({
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: "https://jutsa-management-system.vercel.app/",
-  })
-);
-// Middleware
-app.use(express.json());
+// app.use(
+//   cors({
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     origin: "https://jutsa-management-system.vercel.app/",
+//   })
+// );
+// // Middleware
+// app.use(express.json());
 
-// Routes
-app.use("/api/users", userRoutes);
-app.use("/api/competitors", competitorRoutes);
+// // Routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/competitors", competitorRoutes);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Error handler middleware
-app.use(errorHandler);
+// // Error handler middleware
+// app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
