208cb9992e0819043f34a5248807f3f084662d62
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-29 16:39:11 +0300

    Refactor finance controller and routes This commit refactors the finance controller and related routes to improve code organization and readability.

--- Backend/controllers/finance-controller.js
+++ Backend/controllers/finance-controller.js
@@ -4,148 +4,149 @@ import prisma from "../config/db.js";
 import asyncHandler from "express-async-handler";
 
 // Create
-export const createFinance = asyncHandler(async(req,res)=>{
-    const { title, amount, type, userId,  category } = req.body;
+export const RegisterFinance = asyncHandler(async (req, res) => {
+  const { title, amount, type, userId, category } = req.body;
 
-    if(!title ||!amount ||!type ||!userId || !category){
-        res.status(400);
-        throw new Error("All fields are required");
-    }
+  if (!title || !amount || !type || !userId || !category) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
 
-    const finance = await prisma.finance.create({
-        data:{
-            title,
-            amount:Number(amount),
-            type,
-            category,
-            userId
-        }
-    })
+  const finance = await prisma.finance.create({
+    data: {
+      title,
+      amount: Number(amount),
+      type,
+      category,
+      userId,
+    },
+  });
 
-    if(!finance){
-        res.status(501);
-        throw new Error("Finance not created");
-    }
+  if (!finance) {
+    res.status(501);
+    throw new Error("Finance not created");
+  }
 
-    res.status(201).json({
-        success: true,
-        error: null,
-        data: finance,
-    });
-})
+  res.status(201).json({
+    success: true,
+    error: null,
+    message: "Register successfully",
+    data: finance,
+  });
+});
 
 // Read financial information
 
-export const getFinances = asyncHandler(async(req,res)=>{
-    const finances = await prisma.finance.findMany()
+export const getFinances = asyncHandler(async (req, res) => {
+  const finances = await prisma.finance.findMany();
 
-    if(finances.length === 0){
-        res.status(404);
-        throw new Error("Unable to find:No finances found");
-    }
+  if (finances.length === 0) {
+    res.status(404);
+    throw new Error("Unable to find:No finances found");
+  }
 
-    res.status(200).json({
-        success: true,
-        error: null,
-        data: finances,
-    });
-})
+  res.status(200).json({
+    success: true,
+    error: null,
+    data: finances,
+  });
+});
 
 // Read single financial information
 
-export const getFinance = asyncHandler(async(req,res)=>{
-    const { id } = req.params;
-    const finance = await prisma.finance.findUnique({
-        where:{
-            id
-        }
-    })
+export const getFinance = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const finance = await prisma.finance.findUnique({
+    where: {
+      id,
+    },
+  });
 
-    if(!finance){
-        res.status(404);
-        throw new Error("Unable to find:No finance found");
-    }
+  if (!finance) {
+    res.status(404);
+    throw new Error("Unable to find:No finance found");
+  }
 
-    res.status(200).json({
-        success: true,
-        error: null,
-        data: finance,
-    });
-})
+  res.status(200).json({
+    success: true,
+    error: null,
+    data: finance,
+  });
+});
 
 // Delete Finance using unique ID
 
-export const deleteFinance = asyncHandler(async(req,res)=>{
-    const { id } = req.params;
+export const deleteFinance = asyncHandler(async (req, res) => {
+  const { id } = req.params;
 
-    if(!ObjectId.isValid(id)){
-        res.status(400);
-        throw new Error("Please provide a valid id");
-    }
+  if (!ObjectId.isValid(id)) {
+    res.status(400);
+    throw new Error("Please provide a valid id");
+  }
 
-    const finance = await prisma.finance.delete({
-        where:{
-            id
-        }
-    })
+  const deletedFinance = await prisma.finance.delete({
+    where: {
+      id,
+    },
+  });
 
-    if(!finance){
-        res.status(501);
-        throw new Error("Unable to delete: unexpected error occurred");
-    }
+  if (!deletedFinance) {
+    res.status(501);
+    throw new Error("Unable to delete: unexpected error occurred");
+  }
 
-    res.status(200).json({
-        success: true,
-        error: null,
-        data: {
-            message: "Deleted successfully",
-        },
-    });
-})
+  res.status(200).json({
+    success: true,
+    error: null,
+    message: "Deleted successfully",
+    data: deletedFinance,
+  });
+});
 
 // Update Finance using unique ID
 
-export const updateFinance = asyncHandler(async(req,res)=>{
-    const { id } = req.params;
-    const {title,amount,type,category,userId} = req.body;
+export const updateFinance = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { title, amount, type, category, userId } = req.body;
 
-    if(!ObjectId.isValid(id)){
-        res.status(400);
-        throw new Error("Please provide a valid id");
-    }
+  if (!ObjectId.isValid(id)) {
+    res.status(400);
+    throw new Error("Please provide a valid id");
+  }
 
-    const isFinanceExists = await prisma.finance.findUnique({
-        where:{
-            id
-        }
-    })
+  const isFinanceExists = await prisma.finance.findUnique({
+    where: {
+      id,
+    },
+  });
 
-    if(!isFinanceExists){
-        res.status(404);
-        throw new Error("Unable to find:No finance found");
-    }
+  if (!isFinanceExists) {
+    res.status(404);
+    throw new Error("Unable to find:No finance found");
+  }
 
-    const updatedFinance = await prisma.finance.update({
-        data:{
-            title: title,
-            amount: Number(amount),
-            type: type,
-            category: category,
-            userId: userId,
-        },
-        where:{
-            id
-        }
-    })
-    
-    if(!updatedFinance){
-        res.status(501);
-        throw new Error("Unable to update: unexpected error occurred");
-    }
+  const updatedFinance = await prisma.finance.update({
+    data: {
+      title: title,
+      amount: Number(amount),
+      type: type,
+      category: category,
+      userId: userId,
+    },
+    where: {
+      id,
+    },
+  });
 
-    res.status(200).json({
-        success: true,
-        error: null,
-        data:updatedFinance
-    });
-})
+  if (!updatedFinance) {
+    res.status(501);
+    throw new Error("Unable to update: unexpected error occurred");
+  }
+
+  res.status(200).json({
+    success: true,
+    error: null,
+    message: "Updated successfully",
+    data: updatedFinance,
+  });
+});
