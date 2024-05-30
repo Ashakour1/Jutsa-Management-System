335c34280369b270a95ff1bd2bd5dbaf1ec97108
Ashakuur1 <engshakrayare114@gmail.com>
2024-05-30 20:28:01 +0300

    feat: Add lazy loading for images in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,37 +7,39 @@ import HeroSection from "./pages/Hero-section";
 import { Toaster, toast } from "sonner";
 import CallToAction from "./pages/Call-to-action";
 import NotFound from "./pages/not-found";
-import About from "./pages/about"
+import About from "./pages/about";
 import RegisterForm from "./component/Register-form";
 import RegisterPage from "./pages/Register-page";
 import FAQPage from "./pages/FAQ-page";
-
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
     <>
       <Toaster richColors position="top-center" />
-      <Router>
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HeroSection />
-                <CallToAction />
-                <Benefits />
-              </>
-            }
-          />
+      <HelmetProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <HeroSection />
+                  <CallToAction />
+                  <Benefits />
+                </>
+              }
+            />
 
-          <Route path="/about" element={<About/>} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/about/faq" element={<FAQPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </Router>
+            <Route path="/about" element={<About />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/about/faq" element={<FAQPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </HelmetProvider>
     </>
   );
 }
