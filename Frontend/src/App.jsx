d87bce5039c19285e97ac886c2cb684c291da705
Ashakuur1 <engshakrayare114@gmail.com>
2024-07-31 20:46:23 +0300

    adding app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,16 @@
-import "./App.css";
-import Footer from "./component/Footer";
-import Benefits from "./pages/benefits";
-import Header from "./component/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HeroSection from "./pages/Hero-section";
-import { Toaster, toast } from "sonner";
-import CallToAction from "./pages/Call-to-action";
-import NotFound from "./pages/not-found";
-import About from "./pages/about";
-import RegisterForm from "./component/Register-form";
-import RegisterPage from "./pages/Register-page";
-import FAQPage from "./pages/FAQ-page";
 import { HelmetProvider } from "react-helmet-async";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
+import "./App.css";
+import About from "./pages/about";
+import Benefits from "./pages/benefits";
+import CallToAction from "./pages/Call-to-action";
+import FAQPage from "./pages/FAQ-page";
+import HeroSection from "./pages/Hero-section";
+import NotFound from "./pages/not-found";
+import RegisterPage from "./pages/Register-page";
 
 // Layout with header and footer
-const MainLayout = ({ children }) => (
-  <>
-    <Header />
-    {children}
-    <Footer />
-  </>
-);
-
-// Layout without header and footer
-const LoginAndSignLayout = ({ children }) => <>{children}</>;
 
 function App() {
   return (
@@ -37,62 +22,17 @@ function App() {
             <Route
               path="/"
               element={
-                <MainLayout>
+                <>
                   <HeroSection />
                   <CallToAction />
-                  <Benefits />
-                </MainLayout>
+                </>
               }
             />
-
-            <Route
-              path="/about"
-              element={
-                <MainLayout>
-                  <About />
-                </MainLayout>
-              }
-            />
-            <Route
-              path="/signup"
-              element={
-                <LoginAndSignLayout>
-                  <Signup />
-                </LoginAndSignLayout>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <LoginAndSignLayout>
-                  <Login />
-                </LoginAndSignLayout>
-              }
-            />
-            <Route
-              path="/register"
-              element={
-                <MainLayout>
-                  <RegisterPage />
-                </MainLayout>
-              }
-            />
-            <Route
-              path="/about/faq"
-              element={
-                <MainLayout>
-                  <FAQPage />
-                </MainLayout>
-              }
-            />
-            <Route
-              path="*"
-              element={
-                <MainLayout>
-                  <NotFound />
-                </MainLayout>
-              }
-            />
+            <Route path="/about" element={<About />} />
+            ء
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/about/faq" element={<FAQPage />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Router>
       </HelmetProvider>
