0ed5d729459be7f2a8c68103d989508eca9f0638
modev <modev.404@gmail.com>
2024-08-12 14:33:19 +0300

    update not-found page and dashboard

--- Admin/src/pages/not-found.jsx
+++ Admin/src/pages/not-found.jsx
@@ -1,26 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
+import { Link, useLocation } from "react-router-dom";
 
 const NotFound = () => {
+  const { pathname } = useLocation()
   return (
     <div>
-      <Helmet>
-        <title>404 - Jutsa</title>
-      </Helmet>
-      <div class="grid h-screen place-content-center bg-white px-4">
-        <div class="text-center">
-          <h1 class="text-9xl font-black text-gray-200">404</h1>
+      <title>404 - Jutsa</title>
+      <div className="grid h-screen place-content-center bg-white px-4">
+        <div className="text-center">
+          <h1 className="text-9xl font-black text-gray-200">404</h1>
 
-          <p class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Uh-oh!
           </p>
 
-          <p class="mt-4 text-gray-500">We can't find that page.</p>
+          <p className="mt-4 text-gray-500">We can't find that page.</p>
 
           <Link
-            to="/"
-            class="mt-6 inline-block rounded bg-customGreen px-5 py-3 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring"
+            to={ pathname.includes("/dashboard") ? "/dashboard" : "/" }
+            className="mt-6 inline-block rounded bg-customGreen px-5 py-3 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring"
           >
             Go Back Home
           </Link>
