c58072bc90a5f97866bac0be3d72e7b0a35ec9c2
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-14 19:58:19 +0300

    Update App.jsx to remove unused imports and components

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,25 +1,20 @@
 import { HelmetProvider } from "react-helmet-async";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Outlet,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import { Toaster } from "sonner";
 import "./App.css";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 import NotFound from "./pages/not-found";
 import Signup from "./pages/Signup";
 
 function App() {
   return (
     <>
-      <Toaster richColors position="top-center" />
-      <HelmetProvider>
-        <Router>
-          <Routes>
-            <Route path="/register" element={<Signup />} />
-            <Route path="/" element={<Dashboard />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </HelmetProvider>
+    
     </>
   );
 }
