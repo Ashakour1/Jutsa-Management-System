5fa81d06ec122eb504a0bcf886f756046bffb4c4
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-14 19:57:51 +0300

    Refactor import statements to use double quotes consistently

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import { router } from './routes/routes.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import { router } from "./routes/routes.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
