629bca6efbe46b3289320533fd6d63f788e668ff
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-14 19:58:06 +0300

    Update routes.jsx to use consistent import statements and remove unused components

--- Admin/src/routes/routes.jsx
+++ Admin/src/routes/routes.jsx
@@ -1,26 +1,15 @@
-import DashboardLayout from "@/components/layout/DashboardLayout";
-import Dashboard from "@/pages/Dashboard";
-import NotFound from "@/pages/not-found";
+import App from "@/App";
+import Login from "@/pages/Login";
 import Signup from "@/pages/Signup";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
-    { 
-      path:"/dashboard",element:<DashboardLayout/>,
-      children:[
-        { index:"/", element:<Dashboard/>},
-        // { path:"/dashboard/sports", },
-        // { path:"/dashboard/sports/create", },
-        // { path:"/dashboard/sports/:id",},
-      ],
-    },
-    {
-      path:"/",element:<Signup/>
-    },
-    {
-      path:"/login",element:<Signup/>
-    },
-    {
-      path:"*",element:<NotFound/>
-    },
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
 ]);
