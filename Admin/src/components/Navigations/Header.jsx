2f816d7693a7ae7acf6dcc992b061254d04a37bf
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-18 15:02:52 +0300

    feat: Add Header component for navigation
    This commit adds a new component called Header to handle the navigation bar. It includes the necessary imports for dropdown menus, input fields, and sheets. The component also defines the routes for the navigation links and renders them accordingly. Additionally, it includes a mobile version of the header for responsive design.

---
+++ Admin/src/components/Navigations/Header.jsx
@@ -0,0 +1,98 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { CircleUser, Menu, Package2, Search } from "lucide-react";
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { Button } from "../ui/button";
+import HeaderMobile from "./Header-mobile";
+export const routes = [
+  {
+    route: "/dashboard",
+    name: "Dashboard",
+  },
+  {
+    route: "/positions",
+    name: "Positions",
+  },
+  {
+    route: "/finance",
+    name: "Finance",
+  },
+  {
+    route: "/sports",
+    name: "Sports",
+  },
+  {
+    route: "/it-day",
+    name: "IT Day",
+  },
+  {
+    route: "/user-management",
+    name: "User Management",
+  },
+];
+const Header = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const [openNav, setOpenNav] = useState(false);
+
+  const openNavBar = () => {
+    setNavIsOpen(!navIsOpen);
+  };
+
+  const closeNavBar = () => {
+    setNavIsOpen(false);
+  };
+
+  return (
+    <div>
+      <header className="sticky top-0 flex justify-between h-16 items-center text-black gap-4 border-b bg-background x-4 md:px-6">
+        <div className=" md:flex gap-5 hidden">
+          <img src="logo.png" alt="logo" className=" w-10 h-10" />
+          <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+            {routes.map((route, index) => (
+              <Link
+                key={index}
+                to={route.route}
+                className="text-foreground transition-colors hover:text-foreground"
+              >
+                {route.name}
+              </Link>
+            ))}
+          </nav>
+        </div>
+
+        <HeaderMobile />
+        <div className="">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </header>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Header;
