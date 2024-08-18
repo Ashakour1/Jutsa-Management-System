a1bbe7e474163b1c39fdacf73e4b9f49253de2ef
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-18 15:07:04 +0300

    refactor: Update routes.jsx to use relative imports

--- Admin/src/routes/routes.jsx
+++ Admin/src/routes/routes.jsx
@@ -1,9 +1,20 @@
-import App from "@/App";
-import Login from "@/pages/Login";
-import Signup from "@/pages/Signup";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
 import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "@/pages/Dashboard/Dashboard";
+import Header from "@/components/Navigations/Header";
 
 export const router = createBrowserRouter([
+  {
+    path: "/dashboard",
+    element: <Header />, // This will render the Header
+    children: [
+      {
+        index: true, // This will match the /dashboard path
+        element: <Dashboard />, // This will render Dashboard inside Header
+      },
+    ],
+  },
   {
     path: "/",
     element: <Login />,
