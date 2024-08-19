12f9d0fa0ca1ae76ad8e448b4608f7edfaee0343
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-19 17:56:06 +0300

    Update logo image source in Header.jsx

--- Admin/src/components/Navigations/Header.jsx
+++ Admin/src/components/Navigations/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
     <div>
       <header className="sticky top-0 flex justify-between h-16 items-center text-black gap-4 border-b bg-background x-4 md:px-6">
         <div className="md:flex gap-5 hidden">
-          <img src="logo.png" alt="logo" className="w-10 h-10" />
+          <img src="/logo.png" alt="logo" className="w-10 h-10" />
           <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
             {routes.map((route, index) => (
               <Link
