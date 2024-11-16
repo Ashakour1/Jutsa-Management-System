3d27c3ea659fa391ac7d5ca60ebf37f5bea94142
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-16 22:21:08 +0300

    add(Contact): adjust main container height and padding for improved layout

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import ContactForm from "../components/jutsa-components/ContactForm";
 
 const Contact = () => {
   return (
-    <main className="max-w-5xl mx-auto px-4 py-20 space-y-5">
+    <main className="h-full max-w-5xl mx-auto px-4 py-32 space-y-5">
       <ContactContent />
       <ContactForm />
     </main>
