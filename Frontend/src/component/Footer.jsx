02435f6655001c33379d2237d33fd44158502eef
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-21 00:15:57 +0300

    update Footer component: change logo image source and adjust size for better responsiveness

--- Frontend/src/component/Footer.jsx
+++ Frontend/src/component/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className="mt-auto">
       <div className="flex flex-col justify-center items-center">
         <img
-          className="h-52 w-64  sm:w-50 sm:h-30"
-          src="https://s3-alpha-sig.figma.com/img/068b/99c0/0830ee1a5638e2be2f69e29a65af3aea?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=m82T6neWemt1r9UDoda~mtWL4iiUvM6llp7YtVWUdhQyrg~YiAsP67xu4cFr7i2n21Y8Itn2zbDuO1d9NbmoYFNVGQguYqFDm598sG6VlIDGEofj28BXesbhpWWVNKOoe4PLyWu8vF9PxkW0z~nOkupGXb970~T7o74w~JmbpbrRidjv9WKIn117xyEmVcKO059xzwRE1r5eFl9HJ3WoCJox16lnQhbqi6EqAR4iaMSbbH1j8ciNVrAs0tmgyTyyNM~w39fGIavhSN~vw2B-JFWtQnqHXnGLDE2d5wVSpY97-cTf44BVR6B7FLkvc22gLgW~VIWlSoGG4lbkyxsWbQ__"
+          className="h-14 w-14  sm:w-14 sm:h-14"
+          src="/logojpg.jpg"
           alt="img"
         />
       </div>
