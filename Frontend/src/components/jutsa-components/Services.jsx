b849da425aa3f31296b75324bff0fae9dd7c793d
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-21 22:19:18 +0300

    add(services): refactor ServiceSection to use icons instead of images and improve layout

--- Frontend/src/components/jutsa-components/Services.jsx
+++ Frontend/src/components/jutsa-components/Services.jsx
@@ -1,35 +1,55 @@
+import { ActivityIcon } from "lucide-react";
+import { CgSupport } from "react-icons/cg";
+import { MdDeveloperMode } from "react-icons/md";
+
 const ServiceSection = () => {
   const services = [
     {
       title: "Academic Support",
       description:
         "Our dedicated team of academic advisors is here to help you succeed in your studies. Whether you need assistance with a specific subject or guidance on study skills, we've got you covered.",
-      imageUrl: "/about_image.jpg",
+      icon: (
+        <CgSupport
+          className="text-customBlue bg-green-200 rounded-full p-2"
+          size={45}
+          name="Academic Support"
+        />
+      ),
     },
     {
       title: "Extracurricular Activities",
       description:
         "Engage in a wide variety of clubs, organizations, and activities, from cultural and social to sports teams. It's a great way to make new friends, develop new skills, and enhance your overall university experience.",
-      imageUrl: "/about_image.jpg",
+      icon: (
+        <ActivityIcon
+          className="text-customBlue bg-green-200 rounded-full p-2"
+          size={45}
+          name="Extracurricular Activities"
+        />
+      ),
     },
     {
       title: "Career Development",
       description:
         "We offer comprehensive career services and resources to help you prepare for the job market. From resume writing to interview skills, we'll equip you with the tools you need to succeed in your future career.",
-      imageUrl: "/about_image.jpg",
+      icon: (
+        <MdDeveloperMode
+          className="text-customBlue bg-green-200 rounded-full p-2"
+          size={45}
+          name="Career Development"
+        />
+      ),
     },
   ];
 
   return (
-    <section className="max-w-[1200px] mx-auto px-4 py-16">
-      <div className="text- mb-12">
-        <h2 className="font-bold mb-2 text-customGreen">
-          Activities and Resources We Offer
-        </h2>
+    <section className="max-w-[1200px] mx-auto px-4 py-16 ">
+      <div className="text-center items-center mb-7">
+        <h2 className="font-semibold mb-2 text-customGreen">Activities</h2>
         <h1 className="text-3xl font-bold mb-2 text-customBlue">
-          Our Activities{" "}
+          Activities and Resources We Offer
         </h1>
-        <p className="mb-8 w-4/5">
+        <p className="text-center max-w-3xl mx-auto">
           Discover the wide range of activities and resources we offer to help
           you succeed in your academic and personal life.
         </p>
@@ -44,20 +64,18 @@ const ServiceSection = () => {
   );
 };
 
-function ServiceCard({ title, description, imageUrl }) {
+function ServiceCard({ title, description, icon }) {
   return (
-    <div className="border border-gray-200 rounded-lg shadow-lg  hover:border-green-500 hover:shadow-green-100 hover:shadow-lg hover:scale-15">
-      <div className="h-48">
-        <img
-          src={imageUrl}
-          alt={title}
-          className="w-full h-full object-cover"
-          loading="lazy"
-        />
-      </div>
-      <div className="p-6">
-        <h4 className="text-lg font-semibold text-gray-800 mb-2">{title}</h4>
-        <p className="text-gray-600">{description}</p>
+    <div className="rounded p-4 ">
+      <div className="space-y-4">
+        {icon}
+
+        <div className="">
+          <h1 className="text-xl text-customBlue font-semibold mb-1">
+            {title}
+          </h1>
+          <p className="text-gray-700 text-sm my-4">{description}</p>
+        </div>
       </div>
     </div>
   );
