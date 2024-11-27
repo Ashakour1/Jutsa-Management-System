e55be917ffe2248b2a556566318acc14da4999cb
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-27 16:32:19 +0300

    add(page): create Contact page with content and form components

---
+++ Frontend/src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactContent from "../components/jutsa-components/ContactContent";
+import ContactForm from "../components/jutsa-components/ContactForm";
+
+const Contact = () => {
+  return (
+    <main className="max-w-5xl mx-auto px-4 py-20 space-y-5">
+      <ContactContent />
+      <ContactForm />
+    </main>
+  );
+};
+
+export default Contact;
