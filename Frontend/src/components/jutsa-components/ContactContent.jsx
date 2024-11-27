71fc096041ba219479e25b4f280964bb716e99fd
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-27 16:31:12 +0300

    add(component): create ContactContent component

---
+++ Frontend/src/components/jutsa-components/ContactContent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContactContent = () => {
+  return (
+    <main className="flex flex-col text-start max-w-5xl space-y-3">
+      <h2 className="text-base font-semibold text-customGreen">Contact</h2>
+      <h1 className="text-4xl font-bold text-customBlue">
+        Contact our support team if you need help.
+      </h1>
+      <p className="text-lg text-gray-500">
+        We’re thrilled to have you connect with our association! Reach out to us
+        in any way you prefer 😊.
+      </p>
+    </main>
+  );
+};
+
+export default ContactContent;
