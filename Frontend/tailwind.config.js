b9cc550d4d71f49d630d99355d52b1b28db9bd81
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-27 16:28:27 +0300

    add(tailwind): enhance configuration with dark mode, border radius, and animations

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,16 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        customGreen: "#2BB77B",
-        customBlue: "#0E103F",
-      },
-      maxWidth: {
-        contain: "1140px",
-      },
-    },
+  	extend: {
+  		colors: {
+  			customGreen: '#2BB77B',
+  			customBlue: '#0E103F'
+  		},
+  		maxWidth: {
+  			contain: '1140px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		animation: {
+  			'shiny-text': 'shiny-text 8s infinite'
+  		},
+  		keyframes: {
+  			'shiny-text': {
+  				'0%, 90%, 100%': {
+  					'background-position': 'calc(-100% - var(--shiny-width)) 0'
+  				},
+  				'30%, 60%': {
+  					'background-position': 'calc(100% + var(--shiny-width)) 0'
+  				}
+  			}
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
