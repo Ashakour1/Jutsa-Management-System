acbf4de25ced9dda7079bcacd790af9af24dd1bf
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-27 16:32:35 +0300

    fix(RegisterPage): update import path for RegisterForm to use 'components' directory

--- Frontend/src/pages/Register-page.jsx
+++ Frontend/src/pages/Register-page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import RegisterForm from "../component/Register-form";
+import RegisterForm from "../components/Register-form";
 import { Helmet } from "react-helmet-async";
 
 const RegisterPage = () => {
