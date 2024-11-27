76f64bdaf4dfe73816c3a575e6f710a4490de7b1
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-27 16:28:44 +0300

    add(components): update import paths for components and add Contact page route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,16 +6,17 @@ import About from "./pages/about";
 import Benefits from "./pages/benefits";
 import CallToAction from "./pages/Call-to-action";
 import FAQPage from "./pages/FAQ-page";
-import HeroSectionItDay from "./component/Hero-section-itday";
+import HeroSectionItDay from "./components/Hero-section-itday";
 import NotFound from "./pages/not-found";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import RegisterPage from "./pages/Register-page";
-import HeroSection from "./component/jutsa-components/HeroSection";
-import ServiceSection from "./component/jutsa-components/Services";
-import TeamSection from "./component/jutsa-components/team-section";
+import HeroSection from "./components/jutsa-components/HeroSection";
+import ServiceSection from "./components/jutsa-components/Services";
+import TeamSection from "./components/jutsa-components/team-section";
 import ItDay from "./pages/It-day";
 import Sports from "./pages/Sports";
+import Contact from "./pages/Contact";
 
 // Layout with header and footer
 
@@ -43,6 +44,7 @@ function App() {
             <Route path="/sports" element={<Sports />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/about/faq" element={<FAQPage />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Router>
