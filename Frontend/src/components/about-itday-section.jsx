cd284fbb8cc23435d77faabacd6cb2c7ae094b4c
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-29 23:04:06 +0300

    refactor(about-itday-section.jsx): update styles and colors for improved visual consistency

--- Frontend/src/components/about-itday-section.jsx
+++ Frontend/src/components/about-itday-section.jsx
@@ -5,10 +5,10 @@ import { TbBrandAmazon } from "react-icons/tb";
 
 const AboutItDaySection = () => {
   return (
-    <section className="py-12 md:py-24" id="about">
-      <div className="container mx-auto px-4 md:px-6 space-y-8">
+    <section className="py-12 md:py-24 bg-gray-100" id="about">
+      <div className="mx-auto max-w-contain px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 space-y-8">
         <div className="text-center space-y-4">
-          <h2 className="text-2xl md:text-3xl font-bold text-black">
+          <h2 className="text-2xl md:text-3xl font-bold text-customBlue">
             About IT-DAY
           </h2>
           <p className="text-gray-500  text-lg max-w-[900px] mx-auto">
@@ -19,24 +19,24 @@ const AboutItDaySection = () => {
         </div>
         <div className="grid md:grid-cols-3 gap-6">
           <div className="rounded-xl border border-gray-800 p-8 shadow-xl  hover:border-customBlue hover:shadow-gray-400 space-y-4">
-            <FaSpinner className="w-8 h-8 text-primary" />
-            <h3 className="text-xl font-bold">Innovation</h3>
+            <FaSpinner className="w-8 h-8 text-customBlue" />
+            <h3 className="text-xl text-customGreen font-bold">Innovation</h3>
             <p className="text-gray-500 ">
               Witness the cutting-edge projects and innovative solutions
               developed by our talented students.
             </p>
           </div>
           <div className="rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-customBlue hover:shadow-gray-400 space-y-4">
-            <TbBrandAmazon className="w-8 h-8 text-primary" />
-            <h3 className="text-xl font-bold">Excellence</h3>
+            <TbBrandAmazon className="w-8 h-8 text-customBlue " />
+            <h3 className="text-xl text-customGreen font-bold">Excellence</h3>
             <p className="text-gray-500 ">
               Celebrate the exceptional achievements and technical prowess of
               our IT students.
             </p>
           </div>
           <div className="rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-customBlue hover:shadow-gray-400 space-y-4">
-            <MdShowChart className="w-8 h-8 text-primary" />
-            <h3 className="text-xl font-bold">Showcase</h3>
+            <MdShowChart className="w-8 h-8 text-customBlue " />
+            <h3 className="text-xl text-customGreen font-bold">Showcase</h3>
             <p className="text-gray-500 ">
               Discover the latest advancements in technology through student
               presentations and demonstrations.
