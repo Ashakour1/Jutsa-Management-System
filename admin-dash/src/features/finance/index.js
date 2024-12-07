4194534c34806997b656b3c0b0f5e14a8aac140e
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-07 19:39:07 +0300

    feat(finance/index.js): add finance leads component with data fetching and UI elements

---
+++ admin-dash/src/features/finance/index.js
@@ -0,0 +1,83 @@
+import TrashIcon from "@heroicons/react/24/outline/TrashIcon";
+import TitleCard from "../../components/Cards/TitleCard";
+import { useNavigate } from "react-router-dom";
+import useFinanceStore from "../../stores/financeStore";
+import { useEffect } from "react";
+
+const TopSideButtons = () => {
+  const Navigate = useNavigate();
+  console.log("Navigate", Navigate);
+  return (
+    <div className="inline-block float-right">
+      <button
+        className="btn px-6 btn-sm normal-case btn-primary"
+        onClick={() => Navigate("/add-finance")}
+      >
+        Add New
+      </button>
+    </div>
+  );
+};
+
+function Leads() {
+  const { financeDetails, loading, error, fetchFinanceDetails } =
+    useFinanceStore();
+  console.log(financeDetails.data);
+
+  useEffect(() => {
+    fetchFinanceDetails();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  return (
+    <>
+      <TitleCard
+        title="Finance"
+        // description="Financial"
+        topMargin="mt-2"
+        TopSideButtons={<TopSideButtons />}
+      >
+        <div className="overflow-x-auto w-full">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Amount</th>
+                <th>Created At</th>
+                <th>Status</th>
+                <th>Assigned To</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {financeDetails.map((k, index) => (
+                <tr>
+                  <td>{k.title}</td>
+                  <td>{k.amount}</td>
+
+                  <td>
+                    <div className="badge badge-primary">{k.type}</div>
+                  </td>
+                  <td>{k.category}</td>
+                  <td>
+                    <button className="btn btn-square btn-ghost">
+                      <TrashIcon className="w-5" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </TitleCard>
+    </>
+  );
+}
+
+export default Leads;
