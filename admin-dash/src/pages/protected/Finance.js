22c13c7e0c1b7d805a2615df64d6e2ce5aed07f8
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-07 19:41:51 +0300

    refactor(finance): replace Leads component with Finance component in InternalPage

--- admin-dash/src/pages/protected/Finance.js
+++ admin-dash/src/pages/protected/Finance.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setPageTitle } from '../../features/common/headerSlice'
-import Leads from '../../features/leads'
+import Finance from '../../features/finance'
 
 function InternalPage(){
     const dispatch = useDispatch()
@@ -12,7 +12,7 @@ function InternalPage(){
 
 
     return(
-        <Leads />
+        <Finance />
     )
 }
 
