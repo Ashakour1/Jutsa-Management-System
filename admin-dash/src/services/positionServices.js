2ab8eef7697c75a01d621c383e2b11a804b5b06d
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-07 19:45:00 +0300

    feat(positions): add service to fetch position details from API

---
+++ admin-dash/src/services/positionServices.js
@@ -0,0 +1,13 @@
+const API = "http://localhost:5000/api/positions";
+
+export const fetchPositionDetailsFromAPI = async () => {
+    const response = await fetch(API);
+  
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+  
+    const data = await response.json();
+    // console.log(data.data); // Log the data to inspect its structure
+    return data.data;
+  };
