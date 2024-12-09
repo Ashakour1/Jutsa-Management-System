17b3deebc19105907d496c950ac30462f13c6092
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-09 22:58:33 +0300

    feat(userRoutes): add authentication and role-based authorization middleware for user routes

--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -13,13 +13,40 @@ import {
   loginUser,
   updateUser,
 } from "../controllers/user-controller.js";
+import authMiddleware from "../middlewares/auth-middleware.js";
+import AuthorizeRole from "../middlewares/role-middleware.js";
 
-router.get("/", getUsers);
-router.post("/auth/reg", UserRegister);
-router.put("/edit/:id", updateUser);
+router.get(
+  "/",
+  authMiddleware,
+  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
+  getUsers
+);
+router.post(
+  "/auth/reg",
+  authMiddleware,
+  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
+  UserRegister
+);
+router.put(
+  "/edit/:id",
+  authMiddleware,
+  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
+  updateUser
+);
 router.post("/auth/login", loginUser);
-router.get("/:id", getUser);
-router.delete("/:id", deleteUser);
+router.get(
+  "/:id",
+  authMiddleware,
+  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
+  getUser
+);
+router.delete(
+  "/:id",
+  authMiddleware,
+  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
+  deleteUser
+);
 
 // Export router
 export default router;
