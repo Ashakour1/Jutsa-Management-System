a28528a32e05fa6cedcfaee22fb3da9d9fc24eb4
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-12 01:02:27 +0300

    feat(finance-services): add delete and create finance details functions with error handling

--- admin-dash/src/services/financeServices.js
+++ admin-dash/src/services/financeServices.js
@@ -2,10 +2,45 @@ export const fetchFinanceDetailsFromAPI = async () => {
   const response = await fetch("http://localhost:5000/api/finances");
 
   if (!response.ok) {
-    throw new Error("Something went wrong");
+    throw new Error("Something went wrong from fetch");
   }
 
   const data = await response.json();
   // console.log(data.data); // Log the data to inspect its structure
   return data.data;
+};
+
+export const deleteFinanceDetails = async (id) => {
+  const response = await fetch(`http://localhost:5000/api/finances/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw new Error("Something went wrong from delete");
+  }
+
+  const data = await response.json();
+  return data.data;
+};
+
+export const createFinance = async (data) => {
+  const response = await fetch(
+    `http://localhost:5000/api/finances/reg`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    },
+    {
+      body: JSON.stringify(data), // Convert the data to a JSON string
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const result = response.json();
+
+  // console.log("result" + result);
+  return result.data;
 };
