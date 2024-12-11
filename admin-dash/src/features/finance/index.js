7c0bad8539c80a9b8145169eb87d5f6d8c5e4343
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-12 01:03:35 +0300

    feat(finance): update Leads component to enhance finance details display and navigation

--- admin-dash/src/features/finance/index.js
+++ admin-dash/src/features/finance/index.js
@@ -1,28 +1,33 @@
 import TrashIcon from "@heroicons/react/24/outline/TrashIcon";
-import TitleCard from "../../components/Cards/TitleCard";
-import { useNavigate } from "react-router-dom";
-import useFinanceStore from "../../stores/financeStore";
 import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import TitleCard from "../../components/Cards/TitleCard";
+import useFinanceStore from "../../stores/financeStore";
 
 const TopSideButtons = () => {
   const Navigate = useNavigate();
-  console.log("Navigate", Navigate);
+  // console.log("Navigate", Navigate);
   return (
     <div className="inline-block float-right">
-      <button
-        className="btn px-6 btn-sm normal-case btn-primary"
-        onClick={() => Navigate("/add-finance")}
-      >
-        Add New
-      </button>
-    </div>
+      <Link to={"/app/finance/add"} >
+    <button
+      className="btn px-6 btn-sm normal-case btn-primary"
+      
+    >
+      Add New
+    </button>
+    </Link>
+  </div>
   );
 };
 
 function Leads() {
-  const { financeDetails, loading, error, fetchFinanceDetails } =
+  const { financeDetails, loading, error, fetchFinanceDetails , deleteFinanceDetails } =
     useFinanceStore();
   console.log(financeDetails.data);
+
+
+  
 
   useEffect(() => {
     fetchFinanceDetails();
@@ -47,17 +52,20 @@ function Leads() {
           <table className="table w-full">
             <thead>
               <tr>
+                <th>ID</th>
                 <th>Title</th>
                 <th>Amount</th>
-                <th>Created At</th>
-                <th>Status</th>
-                <th>Assigned To</th>
-                <th></th>
+                <th>Type</th>
+                <th>Category</th>
+                <th>Transactions Date</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
               {financeDetails.map((k, index) => (
-                <tr>
+                <tr key={k.id}>
+ 
+                  <td>{k.id}</td>
                   <td>{k.title}</td>
                   <td>{k.amount}</td>
 
@@ -65,9 +73,10 @@ function Leads() {
                     <div className="badge badge-primary">{k.type}</div>
                   </td>
                   <td>{k.category}</td>
+                  <td>{k.createdAt.slice(0,10)}</td>
                   <td>
                     <button className="btn btn-square btn-ghost">
-                      <TrashIcon className="w-5" />
+                      <TrashIcon className="w-5" onClick={() => deleteFinanceDetails(k.id)}/>
                     </button>
                   </td>
                 </tr>
@@ -76,6 +85,7 @@ function Leads() {
           </table>
         </div>
       </TitleCard>
+    
     </>
   );
 }
