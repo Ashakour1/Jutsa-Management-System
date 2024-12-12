4d0ab20881e301712145774c998455ee95eaffd8
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-13 01:55:35 +0300

    feat(members): add registerMember function to handle member registration

--- admin-dash/src/services/memberServices.js
+++ admin-dash/src/services/memberServices.js
@@ -1,13 +1,29 @@
 const API = "http://localhost:5000/api/members";
 
 export const fetchMemberDetailsFromAPI = async () => {
-    const response = await fetch(API);
-  
-    if (!response.ok) {
-      throw new Error("Something went wrong");
-    }
-  
-    const data = await response.json();
-    // console.log(data.data); // Log the data to inspect its structure
-    return data.data;
-  };
+  const response = await fetch(API);
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const data = await response.json();
+  // console.log(data.data); // Log the data to inspect its structure
+  return data.data;
+};
+
+export const registerMember = async (formData) => {
+  const response = await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to register member");
+  }
+
+  return response.json();
+};
