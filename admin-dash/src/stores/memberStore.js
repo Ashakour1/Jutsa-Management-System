6453823bb2832b7cd925fdb6245b5f2899a21127
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-13 01:55:58 +0300

    feat(members): add register function to handle member registration

--- admin-dash/src/stores/memberStore.js
+++ admin-dash/src/stores/memberStore.js
@@ -1,5 +1,8 @@
 import { create } from "zustand";
-import { fetchMemberDetailsFromAPI } from "../services/memberServices"
+import {
+  fetchMemberDetailsFromAPI,
+  registerMember,
+} from "../services/memberServices";
 
 const useMemberStore = create((set) => ({
   MemberDetails: [], // Always starts as an empty array
@@ -12,6 +15,22 @@ const useMemberStore = create((set) => ({
     try {
       const data = await fetchMemberDetailsFromAPI();
       set({ MemberDetails: data || [], loading: false }); // Ensure data is always an array
+    } catch (err) {
+      set({ error: err.message, loading: false });
+    }
+  },
+
+  register: async (formData) => {
+    set({ loading: true, error: null });
+
+    try {
+      // Call the API to register the member
+      const result = await registerMember(formData);
+      set((state) => ({
+        // Add the new member to the existing list of members
+        MemberDetails: [...state.MemberDetails, result.data],
+        loading: false,
+      }));
     } catch (err) {
       set({ error: err.message, loading: false });
     }
