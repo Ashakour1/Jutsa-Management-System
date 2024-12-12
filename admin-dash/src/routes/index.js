7fe9e35fead78db2e463be42abbd6a47993b361e
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-12 23:25:50 +0300

    fix(routes): remove unused Welcome route from routing configuration

--- admin-dash/src/routes/index.js
+++ admin-dash/src/routes/index.js
@@ -3,7 +3,6 @@
 import { Children, lazy } from "react";
 
 const Dashboard = lazy(() => import("../pages/protected/Dashboard"));
-const Welcome = lazy(() => import("../pages/protected/Welcome"));
 const Page404 = lazy(() => import("../pages/protected/404"));
 const Blank = lazy(() => import("../pages/protected/Blank"));
 const Charts = lazy(() => import("../pages/protected/Charts"));
@@ -29,18 +28,14 @@ const routes = [
     path: "/dashboard", // the url
     component: Dashboard, // view rendered
   },
-  {
-    path: "/welcome", // the url
-    component: Welcome, // view rendered
-  },
+
   {
     path: "/finance",
     component: Finance,
-    
   },
   {
     path: "/finance/add",
-    component : AddFinance
+    component: AddFinance,
   },
   {
     path: "/sports",
@@ -102,8 +97,6 @@ const routes = [
     path: "/blank",
     component: Blank,
   },
-
-
 ];
 
 export default routes;
