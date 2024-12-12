60f8eaaa745cc28b4cef581fe30b54f19ef0ad5e
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-13 01:56:02 +0300

    feat(positions): add register function to handle position registration

--- admin-dash/src/stores/positionStore.js
+++ admin-dash/src/stores/positionStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { fetchPositionDetailsFromAPI } from "../services/positionServices"
+import { fetchPositionDetailsFromAPI, registerPosition } from "../services/positionServices";
 
 const usePositionStore = create((set) => ({
   PositionsDetails: [], // Always starts as an empty array
@@ -12,6 +12,22 @@ const usePositionStore = create((set) => ({
     try {
       const data = await fetchPositionDetailsFromAPI();
       set({ PositionsDetails: data || [], loading: false }); // Ensure data is always an array
+    } catch (err) {
+      set({ error: err.message, loading: false });
+    }
+  },
+
+  register: async (formData) => {
+    set({ loading: true, error: null });
+
+    try {
+      // Call the API to register the position
+      const result = await registerPosition(formData);
+      set((state) => ({
+        // Add the new position to the existing list of positions
+        PositionsDetails: [...state.PositionsDetails, result.data],
+        loading: false,
+      }));
     } catch (err) {
       set({ error: err.message, loading: false });
     }
