1f8c1fd7d90e42a6176d2b083bd6e745e4d1d466
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-12 23:25:57 +0300

    feat(user): add user login service with API integration

---
+++ admin-dash/src/services/userServices.js
@@ -0,0 +1,18 @@
+const API = "http://localhost:5000/api/users";
+
+export const LoginUser = async (data) => {
+  const response = await fetch(`${API}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("An error occurred while logging in");
+  }
+
+  const result = await response.json();
+  return result;
+};
