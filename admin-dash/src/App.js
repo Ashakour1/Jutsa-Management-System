2e77570bf492e8eb1fa5053b85468d57230e38ba
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-20 20:43:47 +0300

    feat(App): integrate react-hot-toast for improved user notifications

--- admin-dash/src/App.js
+++ admin-dash/src/App.js
@@ -1,62 +1,60 @@
-import React, { lazy, useEffect } from 'react'
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import { themeChange } from 'theme-change'
-import checkAuth from './app/auth';
-import initializeApp from './app/init';
+import React, { lazy, useEffect } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { themeChange } from "theme-change";
+import checkAuth from "./app/auth";
+import initializeApp from "./app/init";
+import { Toaster } from "react-hot-toast";
 
 // Importing pages
-const Layout = lazy(() => import('./containers/Layout'))
-const Login = lazy(() => import('./pages/Login'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
-const Register = lazy(() => import('./pages/Register'))
+const Layout = lazy(() => import("./containers/Layout"));
+const Login = lazy(() => import("./pages/Login"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
+const Register = lazy(() => import("./pages/Register"));
 const Finance = lazy(() => import("./pages/protected/Finance"));
 const AddFinance = lazy(() => import("./pages/protected/Add-finance"));
-const Documentation = lazy(() => import('./pages/Documentation'))
-
-
+const Documentation = lazy(() => import("./pages/Documentation"));
 
 // Initializing different libraries
-initializeApp()
-
+initializeApp();
 
 // Check for login and initialize axios
-const token = checkAuth()
-
+const token = checkAuth();
 
 function App() {
-
   useEffect(() => {
     // 👆 daisy UI themes initialization
-    themeChange(false)
-  }, [])
-
+    themeChange(false);
+  }, []);
 
   return (
     <>
+      <Toaster />
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/register" element={<Register />} />
           <Route path="/documentation" element={<Documentation />} />
-          
+
           {/* Place new routes over this */}
           <Route path="/app/*" element={<Layout />} />
-          
+
           {/* <Route path="/add" element={<AddFinance/>} /> */}
 
           {/* <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />}/> */}
-        
-
 
           {/* Default Route - If no route matches */}
           <Route path="*" element={<Navigate to={"/login"} replace />} />
-
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
