c752e88802f743eca6458df5c8960ed6736de49b
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-26 14:38:07 +0300

    feat(index): wrap App component with UserProvider for user context

--- admin-dash/src/index.js
+++ admin-dash/src/index.js
@@ -1,20 +1,23 @@
-import React,  { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './app/store'
-import { Provider } from 'react-redux'
-import SuspenseContent from './containers/SuspenseContent';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import SuspenseContent from "./containers/SuspenseContent";
+import { UserProvider } from "./hooks/useUser";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <Suspense fallback={<SuspenseContent />}>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </Suspense>
+  <Suspense fallback={<SuspenseContent />}>
+    <Provider store={store}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </Provider>
+  </Suspense>
   // </React.StrictMode>
 );
 
