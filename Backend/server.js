e2bd81452c86333ad501d7704d3f73bb06bdf116
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-02 20:42:25 +0300

    feat(form): add routes for managing form visibility status

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ import memberRoutes from "./routes/member-routes.js";
 import financeRoutes from "./routes/finance-routes.js";
 import SportRoutes from "./routes/sport-routes.js";
 import CaawiyeRoutes from "./routes/caawiye-routes.js";
+import FormRoutes from "./routes/form-status.js";
 import cors from "cors";
 
 dotenv.config();
@@ -31,12 +32,33 @@ app.use("/api/competitors", competitorRoutes);
 app.use("/api/members", memberRoutes);
 app.use("/api/sports", SportRoutes);
 app.use("/api/caawiye", CaawiyeRoutes);
+app.use("/api", FormRoutes);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/finances", financeRoutes);
 
+let formVisibility = { showForm: true };
+
+// Route to get form visibility status
+app.get("/api/form", (req, res) => {
+  res.json(formVisibility); // Return current visibility status
+});
+
+// Route to update form visibility status
+app.post("/api/form", (req, res) => {
+  const { showForm } = req.body;
+
+  if (typeof showForm !== "boolean") {
+    return res.status(400).json({ message: "showForm should be a boolean" });
+  }
+
+  // Update the in-memory form visibility status
+  formVisibility = { showForm };
+
+  res.json({ message: "Form visibility updated successfully" });
+});
 // Error handler middleware
 app.use(errorHandler);
 
