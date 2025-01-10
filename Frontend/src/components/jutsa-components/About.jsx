9d441d3f4d8d568d8dd710b71eca448d9339026f
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-10 20:07:07 +0300

    feat(about-section): update content and improve layout in About section

--- Frontend/src/components/jutsa-components/About.jsx
+++ Frontend/src/components/jutsa-components/About.jsx
@@ -4,20 +4,19 @@ import { Link } from "react-router-dom";
 
 const AboutSection = () => {
   return (
-    <section className="bg-gray-100 ">
-      <div className="flex  flex-col max-w-[1200px] md:h-full py-10 h-full mx-auto">
+    <section className="bg-gray-100">
+      <div className="flex  flex-col max-w-[1200px] px-10 md:h-full py-10 h-full mx-auto">
         <div className="flex flex-col  justify-center items-center">
           <h1 className="max-w-xl text-3xl font-bold text-customBlue">
             About Us
           </h1>
           <p className="max-w-3xl text-gray-500 text-center mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            gravida, nisl nec faucibus vestibulum, nulla nunc varius lectus, nec
-            fermentum nunc felis at turpis. Nullam euismod, nulla ac vehicula
-            ultricies,
+            Learn more about Jutsa and what we do to help students succeed in
+            the tech industry. We are a community of IT students dedicated to
+            helping each other grow and learn.
           </p>
         </div>
-        <div className="p-2   flex flex-col px-4 md:px-0 md:flex-row gap-4 pt-4 justify-center">
+        <div className="p-2   flex flex-col px-4 md:px-0 md:flex-row gap-4 pt-5 justify-center">
           <div className="max-w-md flex-1 flex items-center">
             <img
               src="/hero-image.jpg"
@@ -30,23 +29,22 @@ const AboutSection = () => {
             <div className="space-y-3">
               <h1 className="text-xl font-bold text-customGreen">Who we Are</h1>
               <p className="text-gray-500 mt-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                gravida, nisl nec faucibus vestibulum, nulla nunc varius lectus,
-                nec fermentum nunc felis at turpis. Nullam euismod, nulla ac
-                vehicula ultricies, nulla nunc varius lectus, nec fermentum nunc
-                felis at turpis. Nullam euismod, nulla ac vehicula ultricies,
+                Jutsa is a student association that brings together IT students
+                from Jamhuuriya University of Science and Technology. We are
+                dedicated to fostering a community of collaboration and growth
+                in the field of technology.
               </p>
               <h1 className="text-xl font-bold text-customGreen">What we do</h1>
               <p className="text-gray-500 mt-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                gravida, nisl nec faucibus vestibulum, nulla nunc varius lectus,
-                nec fermentum nunc felis at turpis. Nullam euismod, nulla ac
-                vehicula ultricies,
+                We organize events, workshops, and competitions to help students
+                develop their skills and connect with industry professionals.
+                Our goal is to provide a platform for students to learn, grow,
+                and succeed in the tech industry.
               </p>
               <Link
-                to="/"
+                to="/about"
                 type="button"
-                className="group flex items-center transition ease-in-out  text-customGreen"
+                className="group flex  pt-5 items-center transition ease-in-out  text-customGreen"
               >
                 Learn More
                 <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
