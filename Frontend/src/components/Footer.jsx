32392a01a7d1fa530e10fa809cd4092475770f2c
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-10 20:06:23 +0300

    feat(footer): redesign Footer component with new layout and links

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,23 +1,188 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="mt-auto">
-      <div className="flex flex-col justify-center items-center">
-        <img
-          className="h-14 w-14  sm:w-14 sm:h-14"
-          src="/logojpg.jpg"
-          alt="img"
-        />
-      </div>
-      <div className="text-black font-bold h-10 ">
-        <hr className="border border-1.5 border-gray-800 mr-4 ml-4" />
-      </div>
+    <footer>
+      <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          <div>
+            <div className="text-teal-600">
+              <img src="./logo.png" alt="" className="w-10" />
+            </div>
 
-      <p className="flex flex-col justify-center items-center mb-6 mt-6 text-gray-500">
-        @ 2024 Jutsa All right Reserved
-      </p>
-    </div>
+            <p className="mt-4 max-w-xs text-gray-500">
+              Jamhuriya University Technology Student Association (JUTSA) is a
+              vibrant student organization dedicated to promoting innovation,
+              creativity
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-3">
+            <div>
+              <p className="font-medium text-gray-900">Activities</p>
+
+              <ul className="mt-6 space-y-4 text-sm">
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Student Support{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    Extracurricular Activities{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Career Development{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Academic Support{" "}
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <p className="font-medium text-gray-900">Association</p>
+
+              <ul className="mt-6 space-y-4 text-sm">
+                <li>
+                  <a
+                    href="/about"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    About{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="/about"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Meet the Team{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="/contact"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Contact{" "}
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <p className="font-medium text-gray-900">Helpful Links</p>
+
+              <ul className="mt-6 space-y-4 text-sm">
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Contact{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="/faq"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    FAQs{" "}
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            {/* <div>
+              <p className="font-medium text-gray-900">Legal</p>
+
+              <ul className="mt-6 space-y-4 text-sm">
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Accessibility{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Returns Policy{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Refund Policy{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#"
+                    className="text-gray-700 transition hover:opacity-75"
+                  >
+                    {" "}
+                    Hiring Statistics{" "}
+                  </a>
+                </li>
+              </ul>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      <div className="text-center p-5 border  border-customBlue text-customGreen">
+        <p>
+          Made with <span className="heart-animation">❤️</span> by{" "}
+          <Link className="font-bold" to="https://abdishakur.reliatrusty.com/">
+            Abdishakour Mohamed
+          </Link>
+        </p>
+      </div>
+    </footer>
   );
 };
 
