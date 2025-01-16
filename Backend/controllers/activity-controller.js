f30c28bbd7dd7da9c74b45f7dd3f5f98ffc6352d
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-16 12:48:54 +0300

    feat(activity-controller): implement CRUD operations for activities

---
+++ Backend/controllers/activity-controller.js
@@ -0,0 +1,84 @@
+import AsyncHandler from "express-async-handler";
+import prisma from "../config/db.js";
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Public
+
+export const getAllActivities = AsyncHandler(async (req, res) => {
+  const activity = await prisma.activity.findMany();
+  res.json(activity);
+});
+
+// @desc    Get activity by ID
+// @route   GET /api/activity/:id
+// @access  Public
+
+export const getActivityById = AsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const activity = await prisma.activity.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!activity) {
+    res.status(404);
+    throw new Error("not found");
+  }
+
+  res.status(200).json({
+    success: true,
+    data: activity,
+  });
+});
+
+// @desc    Create a new activity
+// @route   POST /api/activity
+// @access  Private
+
+export const registerActivity = AsyncHandler(async (req, res) => {
+  const { title, description, date, speaker, location, type } = req.body;
+
+  const activity = await prisma.activity.create({
+    data: {
+      title,
+      description,
+      date,
+      speaker,
+      location,
+      type,
+    },
+  });
+
+  res.status(201).json({
+    success: true,
+    data: activity,
+  });
+});
+
+// @desc    Update activity
+
+export const updateActivity = AsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { title, description, date, speaker, location, type } = req.body;
+
+  const activity = await prisma.activity.update({
+    where: {
+      id: id,
+    },
+    data: {
+      title,
+      description,
+      date,
+      speaker,
+      location,
+      type,
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    data: activity,
+  });
+});
