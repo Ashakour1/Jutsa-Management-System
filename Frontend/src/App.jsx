fcecb5370a4b633832fd68bc500f437e0c4853be
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-27 20:20:22 +0300

    feat(candidate): refactor candidate registration to use CandidateRegPage component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,6 +22,7 @@ import EventsSection from "./components/jutsa-components/events";
 import CtaSection from "./components/jutsa-components/cta";
 import Announcement from "./components/jutsa-components/Announcement";
 import CandidateForm from "./components/jutsa-components/CandidateForm";
+import CandidateRegPage from "./pages/candidate-reg";
 
 // Layout with header and footer
 
@@ -50,7 +51,7 @@ function App() {
             <Route path="/it-day" element={<ItDay />} />{" "}
             <Route path="/sports" element={<Sports />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/candidate-reg" element={<CandidateForm />} />
+            <Route path="/candidate-reg" element={<CandidateRegPage />} />
             <Route path="/about/faq" element={<FAQPage />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
