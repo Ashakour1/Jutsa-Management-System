3d4060089abd6f48d017ef99b76c4e35fd62e58c
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-27 20:17:06 +0300

    feat(candidate): create candidate registration page with CandidateForm component

---
+++ Frontend/src/pages/candidate-reg.jsx
@@ -0,0 +1,12 @@
+import CandidateForm from "@/components/jutsa-components/CandidateForm";
+import React from "react";
+
+const candidateRegPage = () => {
+  return (
+    <div className="h-full max-w-5xl mx-auto px-4 py-32 space-y-5">
+      <CandidateForm />
+    </div>
+  );
+};
+
+export default candidateRegPage;
