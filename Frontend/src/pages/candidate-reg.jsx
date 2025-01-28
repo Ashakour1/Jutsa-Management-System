c5fb6dd847f8c84b46d0d7705d2386c050f5b65e
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-28 18:38:49 +0300

    fix(candidate): update layout of CandidateRegPage for improved responsiveness

--- Frontend/src/pages/candidate-reg.jsx
+++ Frontend/src/pages/candidate-reg.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CandidateRegPage = () => {
   return (
-    <div className="">
+    <div className="h-full max-w-4xl mx-auto px-4 py-32">
       <CandidateForm />
     </div>
   );
