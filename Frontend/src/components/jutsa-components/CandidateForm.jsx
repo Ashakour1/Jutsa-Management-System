6b6ac98222f7435a7236463a618d3b021fed6bd7
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-28 17:39:47 +0300

    fix(candidate): remove border class from CandidateForm for cleaner layout

--- Frontend/src/components/jutsa-components/CandidateForm.jsx
+++ Frontend/src/components/jutsa-components/CandidateForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CandidateForm = () => {
   return (
-    <main class="flex min-h-screen items-center justify-center  px-4 border">
+    <main class="flex min-h-screen items-center justify-center  px-4">
       <div class="w-[800px]  rounded-lg mx-auto text-black p-8">
         <h1 class="my-4 text-3xl font-bold tracking-tight text-customBlue">
           President Candidate Application{" "}
