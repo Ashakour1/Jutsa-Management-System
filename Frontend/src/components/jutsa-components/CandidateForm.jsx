a777adddd80f29e6e79de0acf30881e5ca87be41
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-28 18:43:04 +0300

    fix(candidate): make CandidateForm full width for improved layout flexibility

--- Frontend/src/components/jutsa-components/CandidateForm.jsx
+++ Frontend/src/components/jutsa-components/CandidateForm.jsx
@@ -3,7 +3,7 @@ import CandidateContent from "./CandidateContent";
 
 const CandidateForm = () => {
   return (
-    <main class="rounded-lg mx-auto text-black p-8">
+    <main class="w-full rounded-lg mx-auto text-black p-8">
       <h1 class="my-4 text-3xl font-bold tracking-tight text-customBlue">
         President Candidate Application{" "}
       </h1>
