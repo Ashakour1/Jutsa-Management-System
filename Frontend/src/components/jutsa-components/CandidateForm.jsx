47e2ff2462f92061c99f996a45263af450cf25dc
Ashakuur1 <engshakrayare114@gmail.com>
2025-01-28 20:13:42 +0300

    fix(candidate): display error message on failed submission in CandidateForm

--- Frontend/src/components/jutsa-components/CandidateForm.jsx
+++ Frontend/src/components/jutsa-components/CandidateForm.jsx
@@ -36,6 +36,7 @@ const CandidateForm = () => {
       toast.success(response.data.message);
     } catch (error) {
       console.error(error);
+      toast.error(error.response.data.message);
     }
   };
 
