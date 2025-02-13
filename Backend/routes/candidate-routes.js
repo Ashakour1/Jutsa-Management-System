9c6b8901011c4ece457b49dacee616e37f22ad55
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-13 20:30:41 +0300

    fix(candidate-routes): remove role authorization from candidate routes

--- Backend/routes/candidate-routes.js
+++ Backend/routes/candidate-routes.js
@@ -8,33 +8,27 @@ import {
   updateCandidate,
 } from "../controllers/candidate-controller.js";
 import authMiddleware from "../middlewares/auth-middleware.js";
-import AuthorizeRole from "../middlewares/role-middleware.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  authMiddleware,
-  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
-  getAllCandidates
-);
+router.get("/", authMiddleware, getAllCandidates);
 router.post(registerCandidate);
 router.get(
   "/:id",
   authMiddleware,
-  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
+
   getCandidateById
 );
 router.put(
   "/:id",
   authMiddleware,
-  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
+
   updateCandidate
 );
 router.delete(
   "/:id",
   authMiddleware,
-  AuthorizeRole("SUPER_ADMIN", "ADMIN"),
+
   deleteCandidate
 );
 
