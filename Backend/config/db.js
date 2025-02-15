592a3569f2b30eb02165190ee567551856d9ac10
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-15 19:16:03 +0300

    fix(db.js): add console log for database connection confirmation

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,5 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+console.log(process.env.DATABASE_URL, "Database Connected");
+
 export default prisma;
 // This file is for database connection
