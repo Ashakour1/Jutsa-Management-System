7be4019b5a1bd5ded744b6a4b120470628529994
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-16 21:25:32 +0300

    fix(Announcement): update announcement text for clarity

--- Frontend/src/components/jutsa-components/Announcement.jsx
+++ Frontend/src/components/jutsa-components/Announcement.jsx
@@ -5,7 +5,7 @@ const Announcement = () => {
   return (
     <div className="bg-customGreen px-4 py-3 text-white">
       <p className="text-center text-sm font-medium">
-        The presidential candidate application is open.{" "}
+        Candidate application is open.{" "}
         <Link to="/candidate-reg" className="inline-block underline">
           Register Now
         </Link>
