c3a1e085050b44077c1ffec5d91be7b323e55b35
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-16 21:40:28 +0300

    fix(CandidateForm): disable error toast notification on form submission

--- Frontend/src/components/jutsa-components/CandidateForm.jsx
+++ Frontend/src/components/jutsa-components/CandidateForm.jsx
@@ -134,7 +134,7 @@ const CandidateForm = () => {
         campaignPlan: "",
       });
     } catch (error) {
-      toast.error(error.response.data.message);
+      // toast.error(error.response.data.message);
     } finally {
       setIsLoading(false);
     }
