8e53597a0bbf1b7c256f4045216a0f5b66361d95
Ashakuur1 <engshakrayare114@gmail.com>
2025-04-13 17:28:05 +0300

    fix: correct function name and API endpoint in activity services

--- admin-dash/src/services/activityServices.js
+++ admin-dash/src/services/activityServices.js
@@ -1,8 +1,10 @@
-export const FecthActivities = async () => {
+export const FetchActivities = async () => {
   const response = await fetch("http://localhost:5000/api/activities");
   if (!response.ok) throw new Error("Failed to fetch activities");
   const data = await response.json();
-  return data;
+
+  console.log(data.data);
+  return data.data;
 };
 
 export const fetchActivityById = async (id) => {
@@ -43,7 +45,7 @@ export const updateActivity = async (id, activityData) => {
 };
 
 export const deleteActivity = async (id) => {
-  const response = await fetch(`http://localhost:5000/api/activity/${id}`, {
+  const response = await fetch(`http://localhost:5000/api/activities/${id}`, {
     method: "DELETE",
   });
   if (!response.ok) {
