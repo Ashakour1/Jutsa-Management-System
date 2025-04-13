bd67aa06b6a1aeeae45e60a9977fb30a936c2a05
Ashakuur1 <engshakrayare114@gmail.com>
2025-04-13 17:27:55 +0300

    chore: update sidebar routes for improved navigation structure

--- admin-dash/src/routes/sidebar.js
+++ admin-dash/src/routes/sidebar.js
@@ -1,25 +1,18 @@
 /** Icons are imported separatly to reduce build time */
-import BellIcon from "@heroicons/react/24/outline/BellIcon";
-import DocumentTextIcon from "@heroicons/react/24/outline/DocumentTextIcon";
-import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon";
-import TableCellsIcon from "@heroicons/react/24/outline/TableCellsIcon";
-import WalletIcon from "@heroicons/react/24/outline/WalletIcon";
-import CodeBracketSquareIcon from "@heroicons/react/24/outline/CodeBracketSquareIcon";
-import DocumentIcon from "@heroicons/react/24/outline/DocumentIcon";
-import ExclamationTriangleIcon from "@heroicons/react/24/outline/ExclamationTriangleIcon";
-import CalendarDaysIcon from "@heroicons/react/24/outline/CalendarDaysIcon";
-import ArrowRightOnRectangleIcon from "@heroicons/react/24/outline/ArrowRightOnRectangleIcon";
-import UserIcon from "@heroicons/react/24/outline/UserIcon";
 import Cog6ToothIcon from "@heroicons/react/24/outline/Cog6ToothIcon";
-import BoltIcon from "@heroicons/react/24/outline/BoltIcon";
-import ChartBarIcon from "@heroicons/react/24/outline/ChartBarIcon";
-import CurrencyDollarIcon from "@heroicons/react/24/outline/CurrencyDollarIcon";
-import InboxArrowDownIcon from "@heroicons/react/24/outline/InboxArrowDownIcon";
-import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
-import KeyIcon from "@heroicons/react/24/outline/KeyIcon";
 import DocumentDuplicateIcon from "@heroicons/react/24/outline/DocumentDuplicateIcon";
-import { MdOutlineSportsSoccer, MdSupport, MdSupportAgent } from "react-icons/md";
+import DocumentIcon from "@heroicons/react/24/outline/DocumentIcon";
+import DocumentTextIcon from "@heroicons/react/24/outline/DocumentTextIcon";
+import ExclamationTriangleIcon from "@heroicons/react/24/outline/ExclamationTriangleIcon";
+import InboxArrowDownIcon from "@heroicons/react/24/outline/InboxArrowDownIcon";
+import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon";
+import UserIcon from "@heroicons/react/24/outline/UserIcon";
+import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
 import { LuBookMarked } from "react-icons/lu";
+import {
+  MdOutlineSportsSoccer,
+  MdSupportAgent
+} from "react-icons/md";
 import { RiPresentationFill } from "react-icons/ri";
 
 const iconClasses = `h-6 w-6`;
@@ -37,16 +30,15 @@ const routes = [
     name: "Finance", // name that appear in Sidebar
   },
   {
-    path: "/app/charts", // url
-    icon: <ChartBarIcon className={iconClasses} />, // icon component
-    name: "Analytics", // name that appear in Sidebar
+    path: "/app/it-day", //url
+    icon: <LuBookMarked className={submenuIconClasses} />, // icon component
+    name: "Faculty Day", // name that appear in Sidebar
   },
   {
-    path: "/app/calendar", // url
-    icon: <CalendarDaysIcon className={iconClasses} />, // icon component
-    name: "Calendar", // name that appear in Sidebar
+    path: "/app/sports",
+    icon: <MdOutlineSportsSoccer className={submenuIconClasses} />,
+    name: "Sports",
   },
-
   {
     path: "", //no url needed as this has submenu
     icon: <DocumentDuplicateIcon className={`${iconClasses} inline`} />, // icon component
@@ -57,21 +49,13 @@ const routes = [
         icon: <RiPresentationFill className={submenuIconClasses} />,
         name: "Seminars & Workshops",
       },
-      {
-        path: "/app/it-day", //url
-        icon: <LuBookMarked className={submenuIconClasses} />, // icon component
-        name: "Faculty Day", // name that appear in Sidebar
-      },
+
       {
         path: "/app/caawiye", //url
         icon: <MdSupportAgent className={submenuIconClasses} />, // icon component
         name: "Caawiye", // name that appear in Sidebar
       },
-      {
-        path: "/app/sports",
-        icon: <MdOutlineSportsSoccer className={submenuIconClasses} />,
-        name: "Sports",
-      },
+
       {
         path: "/app/blank",
         icon: <DocumentIcon className={submenuIconClasses} />,
@@ -111,33 +95,6 @@ const routes = [
       },
     ],
   },
-  {
-    path: "", //no url needed as this has submenu
-    icon: <DocumentTextIcon className={`${iconClasses} inline`} />, // icon component
-    name: "Documentation", // name that appear in Sidebar
-    submenu: [
-      {
-        path: "/app/getting-started", // url
-        icon: <DocumentTextIcon className={submenuIconClasses} />, // icon component
-        name: "Getting Started", // name that appear in Sidebar
-      },
-      {
-        path: "/app/features",
-        icon: <TableCellsIcon className={submenuIconClasses} />,
-        name: "Features",
-      },
-      {
-        path: "/app/components",
-        icon: <CodeBracketSquareIcon className={submenuIconClasses} />,
-        name: "Components",
-      },
-    ],
-  },
-  // {
-  //   path: "/app/add-finance", // url
-  //   icon: <ArrowRightOnRectangleIcon className={iconClasses} />, // icon component
-  //   name: "", // name that appear in Sidebar
-  // }
 ];
 
 export default routes;
